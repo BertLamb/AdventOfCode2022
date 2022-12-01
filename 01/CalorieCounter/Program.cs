207c4e28032ba1595323e991c920daadfbbd2dd7
Bert Lamb <alamb@pobox.com>
2022-12-01 17:46:37 -0500

    Day 1

---
+++ 01/CalorieCounter/Program.cs
@@ -0,0 +1,30 @@
+﻿using var inputStream = new StreamReader(File.OpenRead(@"C:\temp\input.txt"));
+
+int total = 0;
+int maxTotal = 0;
+string? line;
+while((line = await inputStream.ReadLineAsync()) != null)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        if (total > maxTotal)
+        {
+            maxTotal = total;
+        }
+        total = 0;
+    }
+    else
+    {
+        total += int.Parse(line);
+    }
+}
+
+if (total > 0)
+{
+    if (total > maxTotal)
+    {
+        maxTotal = total;
+    }
+}
+
+Console.WriteLine($"Highest Total: {maxTotal}");
---
+++ 02/CalorieCounter2/Program.cs
@@ -0,0 +1,29 @@
+﻿using var inputStream = new StreamReader(File.OpenRead(@"C:\temp\input.txt"));
+
+var calorieTotals = new List<int>();
+int total = 0;
+string? line;
+while((line = await inputStream.ReadLineAsync()) != null)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        calorieTotals.Add(total);
+        total = 0;
+    }
+    else
+    {
+        total += int.Parse(line);
+    }
+}
+
+if (total > 0)
+{
+    calorieTotals.Add(total);
+}
+
+var sortedTotals = calorieTotals.OrderByDescending(i => i).ToList();
+var max = sortedTotals.First();
+var topThreeTotal = sortedTotals.Take(3).Aggregate((a, b) => a + b); 
+
+Console.WriteLine($"Highest Total: {max}");
+Console.WriteLine($"Top 3 Total: {topThreeTotal}");
