f148364d5f3b991a8c51da55e83c3fc93a6d0468
Bert Lamb <alamb@pobox.com>
2022-12-02 08:28:17 -0500

    Day 2

---
+++ 03/Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using var inputStream = new StreamReader(File.OpenRead(@"C:\temp\input2.txt"));
+
+string? line;
+var score = 0;
+while ((line = inputStream.ReadLine()) != null)
+{
+    // A = X = Rock = 1
+    // B = Y = Paper = 2
+    // C = Z = Scissors = 3
+    line = line.Replace("A", "1").Replace("B","2").Replace("C","3")
+               .Replace("X","1").Replace("Y","2").Replace("Z","3");
+    var round = line.Split(' ');
+    score += int.Parse(round[1]);
+    if (round[0] == round[1])
+    {   // draw
+        score += 3;
+        Console.WriteLine("Draw.");
+    }
+    else if ((round[0] == "3" && round[1] == "1")
+         || (round[0] == "2" && round[1] == "3")
+         || (round[0] == "1" && round[1] == "2"))
+    {   // we won
+        score += 6;
+        Console.WriteLine("Win!");
+    }
+    else
+    {
+        // loss
+        Console.WriteLine("Loss :(");
+    }
+}
+
+Console.WriteLine($"Total Score: {score}");
+
+
---
+++ 04/Task4/Program.cs
@@ -0,0 +1,76 @@
+﻿
+using System.Net.Sockets;
+
+using var inputStream = new StreamReader(File.OpenRead(@"C:\temp\input2.txt"));
+
+string? line;
+var score = 0;
+while ((line = inputStream.ReadLine()) != null)
+{
+    // A = Rock = 1
+    // B = Paper = 2
+    // C = Scissors = 3
+    var round = line.Split(' ');
+    if (round[1] == "X")
+    {   // loss
+        score += LoseAgainst(round[0]);
+        Console.WriteLine("Lose :(");
+    }
+    else if (round[1] == "Y")
+    {
+        score += DrawAgainst(round[0]) + 3;
+        Console.WriteLine("Draw.");
+    }
+    else
+    {
+        score += WinAgainst(round[0]) + 6;
+        Console.WriteLine("Win!");
+    }
+}
+
+Console.WriteLine($"Total Score: {score}");
+
+int LoseAgainst(string a)
+{
+    switch (a)
+    {
+        case "A":
+            return 3;
+        case "B":
+            return 1;
+        case "C":
+            return 2;
+    }
+
+    return 0;
+}
+
+int WinAgainst(string a)
+{
+    switch (a)
+    {
+        case "A":
+            return 2;
+        case "B":
+            return 3;
+        case "C":
+            return 1;
+    }
+
+    return 0;
+}
+
+int DrawAgainst(string a)
+{
+    switch (a)
+    {
+        case "A":
+            return 1;
+        case "B":
+            return 2;
+        case "C":
+            return 3;
+    }
+
+    return 0;
+}
