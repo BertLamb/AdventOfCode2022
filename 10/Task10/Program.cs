8acff137a5b3243b920b275430e685122a43c3df
Bert Lamb <alamb@pobox.com>
2022-12-06 18:21:53 -0500

    Day 6

---
+++ 10/Task10/Program.cs
@@ -0,0 +1,35 @@
+﻿var input = File.ReadAllText(@"C:\temp\input6.txt").AsSpan();
+
+var index = IndexOfFirstPacket(input);
+Console.WriteLine($"Index of first packet: {index}");
+Console.WriteLine($"Characters processed to identify packet: {index + 4}");
+
+
+int IndexOfFirstPacket(ReadOnlySpan<char> input)
+{
+    for (int i = 0; i < input.Length - 4; i++)
+    {
+        var packet = input.Slice(i, 4);
+        if (IsUniqueSpan(packet))
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+bool IsUniqueSpan(ReadOnlySpan<char> span)
+{
+    for (int i = 0; i < span.Length - 1; i++)
+    {
+        for (int j = i + 1; j < span.Length; j++)
+        {
+            if (span[i] == span[j])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
---
+++ 11/Task11/Program.cs
@@ -0,0 +1,35 @@
+﻿var input = File.ReadAllText(@"C:\temp\input6.txt").AsSpan();
+
+var index = IndexOfFirstUniqueBlock(input, 14);
+Console.WriteLine($"Index of first packet: {index}");
+Console.WriteLine($"Characters processed to identify packet: {index + 14}");
+
+
+int IndexOfFirstUniqueBlock(ReadOnlySpan<char> input, int uniqueChars)
+{
+    for (int i = 0; i < input.Length - uniqueChars; i++)
+    {
+        var packet = input.Slice(i, uniqueChars);
+        if (IsUniqueSpan(packet))
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+bool IsUniqueSpan(ReadOnlySpan<char> span)
+{
+    for (int i = 0; i < span.Length - 1; i++)
+    {
+        for (int j = i + 1; j < span.Length; j++)
+        {
+            if (span[i] == span[j])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
